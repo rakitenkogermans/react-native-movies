618314bccee4c60933a61e26c30a8f14bfb8bb8d
rakitenkogermans <rakitenko.germans@gmail.com>
2024-03-22 18:42:48 +0200

    added tab navigator with react-native-vector-icons

--- App.tsx
+++ App.tsx
@@ -3,40 +3,87 @@ import {Genre} from './src/views/Genre/Genre.tsx';
 import {Movie} from './src/views/Movie/Movie.tsx';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {MainStackParamList} from './src/@types/Stacks';
-import {NavigationContainer} from '@react-navigation/native';
+import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
 import {ColorConstants, FontConstants} from './src/constants/StyleConstants.ts';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {User} from './src/views/User/User.tsx';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
+const TabNavigator = createBottomTabNavigator();
 const MainStack = createNativeStackNavigator<MainStackParamList>();
+
+const MovieTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: ColorConstants.background,
+    card: ColorConstants.background,
+  },
+};
+
+const MainStackScreen = () => {
+  return (
+    <MainStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: ColorConstants.background,
+        },
+        headerTintColor: ColorConstants.font,
+        headerTitleStyle: {
+          fontWeight: FontConstants.weightBold,
+        },
+      }}>
+      <MainStack.Screen
+        name="Home"
+        component={Home}
+        options={{title: 'Movie Genres'}}
+      />
+      <MainStack.Screen
+        name="Genre"
+        component={Genre}
+        options={{title: 'Movies'}}
+      />
+      <MainStack.Screen
+        name="Movie"
+        component={Movie}
+        options={({route}) => ({title: route.params.movie.title})}
+      />
+    </MainStack.Navigator>
+  );
+};
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <MainStack.Navigator
+    <NavigationContainer theme={MovieTheme}>
+      <TabNavigator.Navigator
         screenOptions={{
-          headerStyle: {
-            backgroundColor: ColorConstants.background,
-          },
-          headerTintColor: ColorConstants.font,
-          headerTitleStyle: {
-            fontWeight: FontConstants.weightBold,
-          },
+          tabBarInactiveBackgroundColor: ColorConstants.background,
+          tabBarActiveBackgroundColor: ColorConstants.background,
+          tabBarInactiveTintColor: ColorConstants.font,
         }}>
-        <MainStack.Screen
-          name="Home"
-          component={Home}
-          options={{title: 'Movie Genres'}}
+        <TabNavigator.Screen
+          name="Main"
+          component={MainStackScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: () => <Icon name="home" size={20} color="#070f49" />,
+          }}
         />
-        <MainStack.Screen
-          name="Genre"
-          component={Genre}
-          options={{title: 'Movies'}}
+        <TabNavigator.Screen
+          name="User"
+          component={User}
+          options={{
+            headerStyle: {
+              backgroundColor: ColorConstants.background,
+            },
+            headerTintColor: ColorConstants.font,
+            headerTitleStyle: {
+              fontWeight: FontConstants.weightBold,
+            },
+            tabBarIcon: () => <Icon name="user" size={20} color="#070f49" />,
+          }}
         />
-        <MainStack.Screen
-          name="Movie"
-          component={Movie}
-          options={({route}) => ({title: route.params.movie.title})}
-        />
-      </MainStack.Navigator>
+      </TabNavigator.Navigator>
     </NavigationContainer>
   );
 };
